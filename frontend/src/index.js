a911909e035869c4a19842f45b42c14f2ee2a508
ketanjain <ketan.jain@digiprima.co>
2025-02-21 11:26:12 +0530

    changes done in frontend and backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,13 @@
-// src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// // src/index.js
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
